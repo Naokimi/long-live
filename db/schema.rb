4c64b41870545767b90121aa799de3eba1c5d261
Paulo DAlberti <phdalberti@gmail.com>
2019-06-30 22:11:59 +0900

    working testing environment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_28_023758) do
+ActiveRecord::Schema.define(version: 2019_06_28_041947) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 2019_06_28_023758) do
     t.string "category"
     t.string "sub_category"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "weeks", force: :cascade do |t|
+    t.integer "number", default: 1
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
