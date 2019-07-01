6bd4ad9d92987c90febd2f2b10fa2511a711afe0
Paulo DAlberti <phdalberti@gmail.com>
2019-07-01 17:32:07 +0900

    react on rails boilerplate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,4 @@
 import "bootstrap";
+import "./index";
+
+// import index from the React app, e.g. import "../blog"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get "xxxs/:id", to: "pages#home"
+  # namespace :api, defaults: { format: :json } do
+  #  namespace :v1 do
+  #    resources :xxxs, only: [ :index, :show, :create  ]
+  #  end
+  # end
+  # # after run rails g controller api/v1/xxxs and watch video from 40:00
 end
