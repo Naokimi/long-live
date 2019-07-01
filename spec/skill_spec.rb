bd6ba8871217107d81886ad35d3c7fa53ddeac51
Paulo DAlberti <phdalberti@gmail.com>
2019-07-01 14:43:04 +0900

    added react

--- spec/skill_spec.rb
+++ spec/skill_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Skill do
   describe "database" do
-    it "should have 42 skills" do
+    it "has 42 skills" do
       expect(Skill.count).to eq(42)
     end
   end
